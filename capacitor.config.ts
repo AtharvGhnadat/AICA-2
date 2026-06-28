51dd4daebecd7645669a27fd9b0efb31b2c42220
Atharv Ghanadat <atharvgh07@gmail.com>
2026-06-28 16:10:03 +0530

    Update Android to sync directly from Vercel deployment

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.emo.ai',
   appName: 'EmoAI',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    url: 'https://aica-2.vercel.app',
+    cleartext: true
+  }
 };
 
 export default config;
